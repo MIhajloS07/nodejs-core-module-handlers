f05081a118c953416804d7bbcbfc20c2dc6261c1
MIhajloS07 <mihajlostoiljkovic554@gmail.com>
2026-09-15 20:14:07 +0200

    Implementing BufferHandler for working with binary files using buffer module

--- modules/BufferHandler.js
+++ modules/BufferHandler.js
@@ -1,1 +1,42 @@
-const { Buffer } = require('buffer'); // Import the Buffer class from the buffer module
+import { Buffer } from 'buffer'; // Import the Buffer class from the buffer module
+
+// const { Buffer } = require('buffer'); // Import the Buffer class from the buffer module
+
+class BufferHandler {
+    constructor() {
+        this.buffer = null; // Initialize the buffer property to null
+    }
+    allocBuffer(size) {
+        this.buffer = Buffer.alloc(size); // Allocate a new Buffer of the specified size
+        return this.buffer; // Return the allocated Buffer
+    }
+    accessBuffer(index) {
+        if (this.buffer && index >= 0 && index < this.buffer.length) {
+            return this.buffer[index]; // Return the byte at the specified index if valid
+        }
+        return null; // Return null if the buffer is not allocated or the index is out of bounds
+    }
+    createBufferFromString(str) {
+        this.buffer = Buffer.from(str); // Create a new Buffer from the provided string
+        return this.buffer; // Return the created Buffer
+    }
+    createBufferFromArray(arr) {
+        this.buffer = Buffer.from(arr); // Create a new Buffer from the provided array of numbers
+        return this.buffer; // Return the created Buffer
+    }
+    writeToBuffer(data) {
+        if (this.buffer) {
+            this.buffer.write(data); // Write the provided data to the Buffer
+        }
+        return null; // Return null if the buffer is not allocated
+    }
+    printBuffer() {
+        if (this.buffer) {
+            console.log('Buffer:', this.buffer, ', Buffer content:', this.buffer.toString()); // Log the Buffer content to the console
+        } else {
+            console.log('Buffer is not allocated.'); // Log a message if the buffer is not allocated
+        }
+    }
+}
+
+export default BufferHandler; // Export the BufferHandler class for use in other modules
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import FileHandler from './modules/FileHandler.js'; // Import the FileHandler class from the modules directory
-import { Buffer } from 'buffer'; // Import the Buffer class from the buffer module
+import BufferHandler from './modules/BufferHandler.js'; // Import the BufferHandler class from the modules directory
 
 const fileHandler = new FileHandler('example.txt'); // Create a new instance of the FileHandler class
+const bufferHandler = new BufferHandler(); // Create a new instance of the BufferHandler class
 
 // fileHandler.writeFile('Backend', (err, message) => {
 //     if (err) {
@@ -28,16 +29,11 @@ const fileHandler = new FileHandler('example.txt'); // Create a new instance of 
 //     }
 // });
 
-
-const myStrBuffer = Buffer.from('Hello, World!'); // Create a new Buffer from a string
-const myNumBuffer = Buffer.from([
-    1, 3, 5, 7, 9, 44, 67, 89, 100, 200, 255 // hexadecimal values 
-]); // Create a new Buffer from an array of numbers
-
-console.log('Buffer from string:', myStrBuffer.toString()); // Log the Buffer to the console
-console.log('Buffer from numbers:', myNumBuffer.toString()); // Log the Buffer to the console
-
-// Accessing individual bytes in the Buffer
-console.log('First byte of myStrBuffer:', myStrBuffer[0]); // Log the first byte of the Buffer to the console
-
-
+bufferHandler.allocBuffer(10); // Allocate a new Buffer of size 10 
+bufferHandler.writeToBuffer('Hello'); // Write the string 'Hello' to the allocated Buffer
+bufferHandler.printBuffer(); // Print the content of the Buffer to the console
+bufferHandler.createBufferFromString('World'); // Create a new Buffer from the string 'World'
+bufferHandler.printBuffer();
+bufferHandler.createBufferFromArray([1, 2, 3, 4, 5]); // Create a new Buffer from an array of numbers
+bufferHandler.printBuffer(); // Print the content of the Buffer to the console
+console.log('Value at index 2:', bufferHandler.accessBuffer(2)); // Access the byte at index 2 of the Buffer
